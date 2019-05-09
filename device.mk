38daaa3b2c6afcad0bf3498625f0f8e602831cb6
joelh <joelhans@protonmail.com>
2019-05-08 21:57:17 -0400

    Revert "potter: switch to binderized light service"
    This reverts commit a89f9ea520d1bafafae802c528a4a4763a884fb6.

--- device.mk
+++ device.mk
@@ -245,7 +245,7 @@ PRODUCT_COPY_FILES += \
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service.potter \
+    android.hardware.light@2.0-service \
     lights.msm8953
 
 # Display Calibration
